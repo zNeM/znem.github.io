371cccfc7686be7a8e37fcce09588b7d5ce4fff6
Giovanni De Vita <97311568+zNeM@users.noreply.github.com>
2025-02-01 19:35:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sei sicura?",
+    "Davvero sicura??",
+    "Sei proprio positiva?",
+    "Pookie per favore...",
+    "Pensaci su!",
+    "Se dici di no, sarò davvero triste...",
+    "Sarò molto triste...",
+    "Sarò molto molto molto triste...",
+    "Va bene, smetterò di chiedere...",
+    "Sto scherzando, per favore dì di sì! ❤️"
 ];
 
 let messageIndex = 0;
